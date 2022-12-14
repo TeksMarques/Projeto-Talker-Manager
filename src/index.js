8e24180643bf99f15a8a5e2b90ed1ea6626e0f69
TeksMarques <taotaodoce@gmail.com>
2022-12-14 15:50:34 -0300

    req 01

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 app.use(express.json());
-//
+
+app.get('/talker', async (req, res) => {
+  const data = JSON.parse(fs.readFileSync('src/talker.json', 'utf-8'));
+    res.status(200).json(data);
+  });
+
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
